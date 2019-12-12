4cec6d6406017bb28e7bf5b3760c1ca6548c7d38
Dendy Armandiaz <dendy.aziz@gmail.com>
2019-12-12 11:16:39 +0700

    Final (2)

--- resources/views/gempa/indexHarga.blade.php
+++ resources/views/gempa/indexHarga.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.simple')
 @section('content')
-    @php if($lat == -6.54 && $lon == 107.08) $destination['city_name'] = 'Bekasi'; @endphp
+    @php if($lat == -6.54 && $lon == 107.08) {$destination['city_name'] = 'Bekasi'; $destination['city_id'] = 54; } @endphp
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
           integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
